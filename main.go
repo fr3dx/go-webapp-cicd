4d21779820b1ee885cc81b689a825c1f1a74cf94
Ferenc Molnar <ferenc.m@protonmail.com>
2021-04-19 11:03:05 +0200

    v7

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+    "fmt"
+    "html"
+    "log"
+    "net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello from Docker, Ferenc. M")
-}
+func main() {
 
-func main (){
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("0.0.0.0:80", nil))
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hi")
+    })
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+
 }
